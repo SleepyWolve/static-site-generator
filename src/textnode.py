b81af5d2fa12528415f58b93c7ea7d2d54bd8d5f
SleepyWolve <83551917+SleepyWolve@users.noreply.github.com>
2025-03-02 22:23:49 +0200

    backup

---
+++ src/textnode.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal text"
+    BOLD = "bold text"
+    ITALIC = "italic text"
+    CODE = "code block"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode():
+    """docstring for TextNode."""
+    def __init__(self, text, text_type, url):
+        super(TextNode, self).__init__()
+        self.text = text
+        self.text_type = text_type 
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        
+
+
