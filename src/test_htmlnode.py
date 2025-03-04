b4439554e20ce52309f0f7f9c3c8465da4e38940
SleepyWolve <83551917+SleepyWolve@users.noreply.github.com>
2025-03-04 23:27:17 +0200

    Add test case

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -18,6 +18,9 @@ class TestTextNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    def test_link(self):
+        node = LeafNode("p", "Hello, world!", {"href": "boot.dev" })
+        self.assertEqual(node.to_html(), '<p href="boot.dev">Hello, world!</p>')
     def test_error(self):
         with self.assertRaises(ValueError):
             LeafNode("p", None)
